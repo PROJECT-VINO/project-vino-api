30b348b8ef53500f81b27c77ef71440e7c336be6
aop225 <aop22@outlook.com>
2023-11-02 19:21:52 -0400

    step 2

---
+++ Project.Vino.Api/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Project.Vino.Domain.Catalog;
+
+namespace Jet.Piranha.Api.Conrollers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            var items = new List<Item>()
+            {
+                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+            };
+
+            return Ok(items);
+        }
+    }
+}
