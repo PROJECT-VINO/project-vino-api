5596db5f6ed4db57bc42a3e7f2e9d335d2e20fb1
GracieHorn <gracieahorn@gmail.com>
2023-11-04 18:46:48 -0500

    Create Database Context

---
+++ Project.Vino.Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Project.Vino.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Vino.Data
+{
+
+    public class StoreContext : DbContext
+    {
+     public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+     { }
+
+     public DbSet<Item> Items { get; set; }
+    }
+}
