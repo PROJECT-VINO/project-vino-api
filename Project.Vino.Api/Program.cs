11b8db9090c4364c6fdfc2c27213cf77359d6af9
GracieHorn <gracieahorn@gmail.com>
2023-12-10 14:19:31 -0500

    update url

--- Project.Vino.Api/Program.cs
+++ Project.Vino.Api/Program.cs
@@ -44,7 +44,8 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
     {
-        builder.WithOrigins("http://localhost:3000")
+        builder.WithOrigins("http://localhost:3000",
+        "https://jolly-sea-0e1972b0f.4.azurestaticapps.net")
         .AllowAnyHeader()
         .AllowAnyMethod();
     });
